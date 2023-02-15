c5ce3a502af264d3231d071965b46a7cfa95053d
Otabek Rakhimov <rahimovmail@gmail.com>
2023-02-15 11:51:55 -0800

    Creat cartSlice function and Navbar component

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -4,4 +4,6 @@ const initialState = { cartItems: [], amount: 0, total: 0, isLoading: true };
 
 const cartSlice = createSlice({ name: "cart", initialState });
 
-console.log(cartSlice);
+// console.log(cartSlice);
+
+export default cartSlice.reducer;
