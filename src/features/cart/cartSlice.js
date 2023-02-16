adc2d1205086e2d2d5129583e6b78bbf6a05c277
Otabek Rakhimov <rahimovmail@gmail.com>
2023-02-15 18:21:20 -0800

    Create cart container and cartitem functions

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,6 +1,12 @@
 import { createSlice } from "@reduxjs/toolkit";
+import cartItems from "../../cartItems";
 
-const initialState = { cartItems: [], amount: 0, total: 0, isLoading: true };
+const initialState = {
+  cartItems: cartItems,
+  amount: 0,
+  total: 0,
+  isLoading: true,
+};
 
 const cartSlice = createSlice({ name: "cart", initialState });
 
